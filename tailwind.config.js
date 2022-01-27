ed8353aa8a7ab9dfa4d15f600ef1d81f5774a865
Shan Yu Lin <ineslin35@gmail.com>
2022-01-27 20:05:09 +0800

    feat: add the customized color in tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,14 @@ module.exports = {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        roseRed: "#922C40",
+        desertSun: "#DC9750",
+        champagne: "#F3EAC0",
+        darkBlue: "#1E2640"
+      },
+    },
   },
   plugins: [],
 }
