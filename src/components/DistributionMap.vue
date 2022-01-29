a1eddd287deed15e919b0435cd98ea2834c50c22
Shan Yu Lin <ineslin35@gmail.com>
2022-01-29 20:48:16 +0800

    feat: add the draft animation for teh opening words

--- src/components/DistributionMap.vue
+++ src/components/DistributionMap.vue
@@ -1,3 +1,56 @@
 <template>
-    <h1>this is the map</h1>
+  <div class="my-0 mx-auto w-10/12">
+    <h1 class="text-4xl">
+      <span id="opening-words"></span>
+      <span v-if="hasTypingDash" class="text-roseRed font-black">_</span>
+    </h1>
+    <h2 id="quote-from" class="mt-4 text-3xl text-right"></h2>
+  </div>
 </template>
+
+<script lang="ts">
+import { defineComponent, ref } from "vue";
+
+export default defineComponent({
+  setup() {
+    const openingWords = ref("口口聲聲說愛臺灣，連臺灣有什麼都不知道。");
+    const quoteFrom = ref("──呂氏攀蜥發現者，呂光洋教授");
+    const speed: number = 300;
+    const wordCount: number = ref(0);
+    const hasTypingDash: boolean = ref(true);
+    return { openingWords, speed, wordCount, quoteFrom, hasTypingDash };
+  },
+  mounted() {
+    this.runOpeningWords();
+  },
+  methods: {
+    plusWordCount() {
+      this.wordCount += 1;
+    },
+    runOpeningWords() {
+      let phrase: string = this.openingWords;
+      if (this.wordCount < phrase.length) {
+        (<HTMLInputElement>(
+          document.getElementById("opening-words")
+        )).innerHTML += phrase.charAt(this.wordCount);
+        this.plusWordCount();
+        setTimeout(() => this.runOpeningWords(), this.speed);
+      } else {
+        this.hasTypingDash = false;
+        this.wordCount = 0;
+        this.runQuoteFrom();
+      }
+    },
+    runQuoteFrom() {
+      let phrase: string = this.quoteFrom;
+      if (this.wordCount < phrase.length) {
+        (<HTMLInputElement>document.getElementById("quote-from")).innerText +=
+          phrase.charAt(this.wordCount);
+        this.plusWordCount();
+        setTimeout(() => this.runQuoteFrom(), this.speed);
+      }
+    },
+  },
+});
+</script>
+
--- src/components/LanguageButtons.vue
+++ src/components/LanguageButtons.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex justify-around mx-0 my-auto pt-4 pb-4 w-4/6 text-white">
+  <div class="flex justify-around my-0 mx-auto pt-4 pb-4 w-4/6 text-white">
     <button class="border rounded-lg p-2 w-20">中文</button>
     <button class="border rounded-lg p-2 w-20">Tâi-bûn</button>
   </div>
