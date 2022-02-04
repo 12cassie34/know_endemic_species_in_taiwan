62d92ad87e8274ecc17b93635a8cce3958e83379
Shan Yu Lin <ineslin35@gmail.com>
2022-02-04 22:51:13 +0800

    feat: add a mock API to fetch menus's text

--- src/components/TheSidebar.vue
+++ src/components/TheSidebar.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
 import LanguageButtons from './LanguageButtons.vue';
+import TheMenu from './TheMenu.vue';
 </script>
 
 <template>
 <div class="bg-darkBlue h-full">
     <LanguageButtons />
+    <TheMenu />
 </div>
     
 </template>
